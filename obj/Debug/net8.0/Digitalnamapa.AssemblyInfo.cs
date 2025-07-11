d404bf8615a12bbb1ac0e821ba44f58c33257087
drazenpanic <drazen.panic@pantim.rs>
2025-07-11 13:49:50 +0200

    ds

--- obj/Debug/net8.0/Digitalnamapa.AssemblyInfo.cs
+++ obj/Debug/net8.0/Digitalnamapa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Digitalnamapa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9ddf627aa9b1d158c8d1fcd7e66de997664820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e812e2c9caa98497c2d44a40af4e65136cad4130")]
 [assembly: System.Reflection.AssemblyProductAttribute("Digitalnamapa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Digitalnamapa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
